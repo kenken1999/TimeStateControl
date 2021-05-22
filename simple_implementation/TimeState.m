f57074dc4de4f97a5da50173130e318778a02f30
ken <ken@email.com>
2021-05-22 16:47:04 +0900

    put robot's angle by the red point

--- simple_implementation/TimeState.m
+++ simple_implementation/TimeState.m
@@ -1,21 +1,24 @@
 clear;
 close all;
 
-dt = 0.1; %%時間刻み=離散時間Tsとして使用
-Tfin = 50; %シミュレーション終了時間
+dt = 0.03; %%時間刻み=離散時間Tsとして使用
+Tfin = 10; %シミュレーション終了時間
 t1 = [0:dt:Tfin];
 
-v1 = ones(1,length(t1))*5;
-v2 = ones(1,length(t1));
+u1 = ones(1,length(t1)) * 1;
+u2 = ones(1,length(t1)) * 1;
 
 zi = zeros(length(t1),3);
 zi(1,:) = [-4 0 5]; %状態z = [z1, z2, z3]T の初期値を設定
 
-k2 = 2;
-k3 = 3;
+v1 = u1 * cos(atan(zi(1,2)));
+v2 = u2 / (cos(atan(zi(1,2))) * cos(atan(zi(1,2))));
 
-x = zi(1,1)+0.5*cos(zi(1,2));
-y = zi(1,3)+0.5*sin(zi(1,2));
+k2 = 3;
+k3 = 4;
+
+x = zi(1,1) + 0.5 * cos(atan(zi(1,2)));
+y = zi(1,3) + 0.5 * sin(atan(zi(1,2)));
 
 hold on;
 axis equal;
@@ -30,24 +33,38 @@ h2 = plot(x,y,'o', 'MarkerSize' ,5, 'MarkerFaceColor', 'r');
 plot(-4,5,'kx','MarkerSize', 10,'LineWidth',2)
 plot(0,0,'rx','MarkerSize', 10,'LineWidth',2)
 
-for i = 1:length(t1)-1
+pause(3)
 
-    v1(i) = -0.1*zi(i,1); %入力v1(=u1cosθ), v1=-λz1で(λ>0の定数)z1を0に収束
+for i = 1:length(t1)-1
 
     %入力v2はv1正負で場合分け
     if v1(i) > 0
-        v2(i) = -k2*zi(i,2)*v1(i)-k3*zi(i,3)*v1(i); 
+
+        v1(i) = -0.5 * zi(i,1); %入力v1(=u1cosθ), v1=-λz1で(λ>0の定数)z1を0に収束
+        v2(i) = -k2 * zi(i,2) * v1(i) - k3 * zi(i,3) * v1(i); 
+
+        zi(i+1,1) = zi(i,1) + v1(i) * dt; %dz1 = v1(i)dt
+        zi(i+1,2) = zi(i,2) + v2(i) * dt; %v2(i)/vi(1)*v1(i)*dt
+        zi(i+1,3) = zi(i,3) + zi(i,2) * v1(i) * dt;
+
     else
-        v2(i) = k2*zi(i,2)*v1(i)-k3*zi(i,3)*v1(i); 
+        
+        zi(i,1) = -zi(i,1);
+
+        v1(i) = -0.5 * zi(i,1); %入力v1(=u1cosθ), v1=-λz1で(λ>0の定数)z1を0に収束
+        v2(i) = k2 * zi(i,2) * v1(i) - k3 * zi(i,3) * v1(i); 
+        
+        zi(i+1,1) = zi(i,1) + v1(i) * dt; %dz1 = v1(i)dt
+        zi(i+1,2) = zi(i,2) - v2(i) * dt; %v2(i)/vi(1)*v1(i)*dt
+        zi(i+1,3) = zi(i,3) - zi(i,2) * v1(i) * dt;
+
     end
+
     
-    zi(i+1,1) = zi(i,1) + v1(i)*dt; %dz1 = v1(i)dt
-    zi(i+1,2) = zi(i,2) + v2(i)*dt; %v2(i)/vi(1)*v1(i)*dt
-    zi(i+1,3) = zi(i,3) + zi(i,2)*v1(i)*dt;
 
     set(h, 'XData', zi(i,1),'YData', zi(i,3));
 
-    set(h2, 'XData', zi(i,1)+0.5*cos(zi(i,2)),'YData', zi(i,3)+0.5*sin(zi(i,2)));
+    set(h2, 'XData', zi(i,1) + 0.5 * cos(atan(zi(i,2))),'YData', zi(i,3) + 0.5 * sin(atan(zi(i,2))));
 
     drawnow;
 
--- simple_implementation/TwoWheels.m
+++ simple_implementation/TwoWheels.m
@@ -2,29 +2,35 @@ clear;
 close all;
 
 dt = 0.01;%%時間刻み=離散時間Tsとして使用
-Tfin = 5;%シミュレーション終了時間
+Tfin = 1.5;%シミュレーション終了時間
 t1 = [0:dt:Tfin];
 
-u1 = ones(1,length(t1))*10;
-u2 = ones(1,length(t1))*10;
+u1 = ones(1,length(t1)) * 10;
+u2 = ones(1,length(t1)) * 10;
 
 xi_z = zeros(length(t1),3);
 xi_z(1,:) = [0 0 0];%状態ξの初期値を設定
 
-h = plot(xi_z(1,1),xi_z(1,2), 'o', 'MarkerSize' ,20, 'MarkerFaceColor', 'b');
+x = xi_z(1,1) + 0.15 * cos(xi_z(1,3));
+y = xi_z(1,2) + 0.15 * sin(xi_z(1,3));
 
 hold on;
 axis equal;
 grid on;
+
 axis([-1.5 1.5 -0.5 2.5])
+
+h = plot(xi_z(1,1),xi_z(1,2), 'o', 'MarkerSize' ,20, 'MarkerFaceColor', 'b');
+h2 = plot(x,y,'o', 'MarkerSize' ,5, 'MarkerFaceColor', 'r');
 
 for i = 1:length(t1)-1
     %θ(n),すなわち xi_z(i+1,3) をx(n),y(n)の計算に使う
-    xi_z(i+1,3) = xi_z(i,3) + u2(i)*dt;
-    xi_z(i+1,1) = xi_z(i,1) + u1(i)*cos(xi_z(i+1,3))*dt;
-    xi_z(i+1,2) = xi_z(i,2) + u1(i)*sin(xi_z(i+1,3))*dt;
+    xi_z(i+1,3) = xi_z(i,3) + u2(i) * dt;
+    xi_z(i+1,1) = xi_z(i,1) + u1(i) * cos(xi_z(i+1,3)) * dt;
+    xi_z(i+1,2) = xi_z(i,2) + u1(i) * sin(xi_z(i+1,3)) * dt;
 
     set(h, 'XData', xi_z(i,1),'YData', xi_z(i,2));
+    set(h2, 'XData', xi_z(i,1) + 0.15 * cos(xi_z(i,3)),'YData', xi_z(i,2) + 0.15 * sin(xi_z(i,3)));
 
     drawnow;
 
