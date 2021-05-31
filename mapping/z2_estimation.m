8342d41a063b06e6c4496d595e32d8cfd157bf85
Ken <ken@example.com>
2021-05-31 17:11:57 +0900

    modify z2_estimation

--- mapping/z2_estimation.m
+++ mapping/z2_estimation.m
@@ -2,7 +2,7 @@ clear;
 close all;
 
 dk = 0.1; %%時間刻み
-Kfin = 10; %シミュレーション終了時間
+Kfin = 5; %シミュレーション終了時間
 k = [0:dk:Kfin];
 
 u1 = ones(1,length(k)) * 5;
@@ -13,31 +13,68 @@ si(1,:) = [0 0 0]; %(s1, s2, s3)=(x ,y, θ)の初期値を設定
 
 zi = zeros(length(k),3); %変換後の状態変数 (z1, z2, z3)=(x, tanθ, y)
 
+p_now = zeros(1,length(k));
+
+alpha = sym('alpha',[1 length(k)])
+
+sigma = 0.01; %スケーリング定数
+p = 0;
+E = 0;
 
 for j = 1:length(k)-1
 
-    zi(j,1) = si(j,1); %z1=s1は既知
-    zi(j,3) = si(j,2); %z3=s2は既知
+    si(j+1,3) = si(j,3) + u2(j) * dk;
+    si(j+1,1) = si(j,1) + u1(j) * cos(si(j+1,3)) * dk;
+    si(j+1,2) = si(j,2) + u1(j) * sin(si(j+1,3)) * dk;
 
-    sigma = 0.01; %スケーリング定数
+    zi(j+1,1) = si(j+1,1); %z1=s1は既知
+    zi(j+1,3) = si(j+1,2); %z3=s2は既知
 
-    p(j) = floor(si(j,3) / sigma); % p = 時刻kのi
-    u = si(j,3) / sigma  - p(j);
+    p_now(j+1) = floor(si(j+1,3) / sigma); % p = 時刻kのi
+    u = si(j+1,3) / sigma  - p_now(j+1);
 
-    alpha(j) = a * p(j) ^ 3 + b * p(j) ^ 2 + c * p(j) + d;
-
-    zi(j,2) = alpha(j) - u * (alpha(j+1) - alpha(j));
-
-    E(j+1) = E(j) + (zi(j,2) - (z3(j+1,3)- z3(j,3)) / (z1(j+1,3)- z1(j,3))) ^ 2;
-
-    if p(j+1) > p(j)
-
-        E(j+1) = E(j+1) + alpha()
-    
+    if p_now(j+1) > p_now(j)
+        p = p + 1;
     end
 
-    si(j+1,3) = si(j,3) + u2(j) * dt;
-    si(j+1,1) = si(j,1) + u1(j) * cos(si(j+1,3)) * dt;
-    si(j+1,2) = si(j,2) + u1(j) * sin(si(j+1,3)) * dt;
+    %zi(j,2) = alpha(p) + u * (alpha(p+1) - alpha(p)); %z2=f(s3)
+
+    E = E + (alpha(p) + u * (alpha(p+1) - alpha(p)) - (zi(j+1,3)- zi(j,3)) / (zi(j+1,1)- zi(j,1))) ^ 2;
+
+    
 
 end
+
+E
+
+for i = 2:p
+
+    E = E + (alpha(i+1) - 2 * alpha(i) + alpha(i-1)) ^ 2;
+
+end
+
+E
+
+eta = 0.01; %学習率
+iteration = 3; %繰り返し回数（最大）
+
+param = ones(iteration,p);
+
+for t=1:iteration-1
+
+    for m = 1:p-1
+
+        syms E
+
+        Df = diff(E,alpha(m))
+
+        Df2 = Df(param(t,m));
+        param(t+1,m) = param(t,m) - eta * double(Df2);
+
+    end
+
+end
+
+param(iteration,:)
+
+
